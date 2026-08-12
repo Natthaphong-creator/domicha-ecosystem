cec28c2132680c004c52dff784be732fda39f140
Natthaphong-creator <nwuttha@gmail.com>
2026-08-12 11:11:33 +0700

    Add back office setup role step

---
+++ supabase/back_office_setup_step_1_roles.sql
@@ -0,0 +1,3 @@
+alter type public.user_role add value if not exists 'Executive';
+alter type public.user_role add value if not exists 'Manager';
+alter type public.user_role add value if not exists 'AssistantManager';
